d2cff8d425e1fd2626ab6092f93abaaa3de22e4e
Rohit Anand <rohanand@microsoft.com>
2020-03-19 19:56:13 +0530

    Fixing Bug

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -94,6 +94,7 @@ namespace StarChart.Controllers
             return NoContent();
         }
 
+        [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id);
